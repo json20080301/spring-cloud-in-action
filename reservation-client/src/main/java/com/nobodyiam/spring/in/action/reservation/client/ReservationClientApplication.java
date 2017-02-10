d37bece45cc5724445115cb1d2a73dc7e1cbe191
json20080301 <15874857042@163.com>
2017-02-10 14:42:53 +0800

    Update ReservationClientApplication.java

--- reservation-client/src/main/java/com/nobodyiam/spring/in/action/reservation/client/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/nobodyiam/spring/in/action/reservation/client/ReservationClientApplication.java
@@ -1,8 +1,11 @@
 package com.nobodyiam.spring.in.action.reservation.client;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -20,34 +23,39 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
-  @Bean
-  CommandLineRunner runner(DiscoveryClient dc) {
-    return args -> {
-      dc.getInstances("reservation-service")
-              .forEach(si -> System.out.println(String.format(
-                      "Found %s %s:%s", si.getServiceId(), si.getHost(), si.getPort())));
-    };
-  }
+	@Bean
+	CommandLineRunner runner(DiscoveryClient dc) {
+		final List<ServiceInstance> list = dc.getInstances("reservation-service");
+		return new CommandLineRunner (){
+			@Override
+			public void run(String... paramArrayOfString) throws Exception {
+				for (ServiceInstance si : list) {
+					System.out.println(String.format("Found %s %s:%s", si.getServiceId(), si.getHost(), si.getPort()));
+				}
+			}
+		};
+			 
+	}
 
-  /**
-   * The load balanced rest template
-   */
-  @LoadBalanced
-  @Bean
-  RestTemplate loadBalanced() {
-    return new RestTemplate();
-  }
+	/**
+	 * The load balanced rest template
+	 */
+	@LoadBalanced
+	@Bean
+	RestTemplate loadBalanced() {
+		return new RestTemplate();
+	}
 
-  /**
-   * The normal rest template
-   */
-  @Primary
-  @Bean
-  RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+	/**
+	 * The normal rest template
+	 */
+	@Primary
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(ReservationClientApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
 }
